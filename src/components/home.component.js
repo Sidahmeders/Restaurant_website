c18a15698cddab64c0a747cab02c32c354d7fc5f
sidahmed boutaraa <ahorob57@gmail.com>
2020-02-01 16:22:25 +0100

    all section completed

--- src/components/home.component.js
+++ src/components/home.component.js
@@ -1,12 +1,24 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
-import '../styles/intro&navbar.css'
-import breakfest from '../resto-img/breakfest.jpg'
-import dishes from '../resto-img/dishes.jpg'
-import cook from '../resto-img/wine.jpg'
-import chef from '../resto-img/chef.jpg'
+import '../styles/intro&navbar.css';
+import breakfest from '../resto-img/breakfest.jpg';
+import dishes from '../resto-img/dishes.jpg';
+import cook from '../resto-img/wine.jpg';
+import chef from '../resto-img/chef.jpg';
+
+import beef from '../resto-img/nested-img/beef.jpg';
+import zouj from '../resto-img/zouj.svg';
+import handHold from '../resto-img/nested-img/hand-hold.jpg';
+import gathering from '../resto-img/nested-img/gathering.jpg';
+import chairs from '../resto-img/nested-img/chair-dining.jpg';
+
+import ReactMapGl, { Marker, NavigationControl, Popup } from 'react-map-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+import zoujAdr from '../resto-img/nested-img/zoujadr.svg';
+import couple from '../resto-img/nested-img/couple.jpg';
+
 
 const IntroNavbar = () => {
 
@@ -37,6 +49,32 @@ const IntroNavbar = () => {
             toggle = true;
         }
     }
+
+    useEffect(() => {
+        const burger = document.getElementById('burger');
+        const listItem = [...burger.getElementsByTagName("li")];
+        const sticky = burger.offsetTop;
+
+        window.onscroll = function() {
+            listItem.forEach(item => {
+                if(window.pageYOffset > sticky - 50) {
+                    item.classList.add('close-burger');
+                } else {
+                    item.classList.remove('close-burger');
+                } 
+            });
+        }  
+    },[]);
+
+    const [viewPort, setViewPort] = useState({
+        latitude: 45.4208,
+        longitude: -75.6940,
+        width: "100vw",
+        height: "100vh",
+        zoom: 16
+      });
+
+    const [selecedHome, setSelectedHome] = useState(null);
 
     return(
         <div className="app">
@@ -72,16 +110,16 @@ const IntroNavbar = () => {
                     </a>
                     
                     <ul>
-                        <li><a href="/side"><i className="fa fa-th-list"></i>Menus</a></li>
-                        <li><a href="/side"><i className="fa fa-images"></i>Gallery</a></li>
-                        <li><a href="/side"><i className="fa fa-carrot"></i>Services</a></li>
-                        <li><a href="/side"><i className="fa fa-users"></i>About Us</a></li>
-                        <li><a href="/side"><i className="fa fa-phone"></i>Booking</a></li>
+                        <li><a href="#menu"><i className="fa fa-th-list"></i>Menus</a></li>
+                        <li><a href="#gallery"><i className="fa fa-images"></i>Gallery</a></li>
+                        <li><a href="#sercices"><i className="fa fa-carrot"></i>Services</a></li>
+                        <li><a href="#about"><i className="fa fa-users"></i>About Us</a></li>
+                        <li><a href="#booking"><i className="fa fa-phone"></i>Booking</a></li>
                     </ul>
 
                     <div className="p">
-                        <p>chlef oulad mohamed</p>
-                        <p>Rue de salam chorfa</p>
+                        <p>Ottawa Gloucester St</p>
+                        <p>Rue Laurier Ave W 48</p>
                         <p>email: habazoj@gmail.com</p>
                         <p>phone: 0772670416</p>
                     </div>
@@ -106,7 +144,7 @@ const IntroNavbar = () => {
                                 <div className="slide">
                                     <img src={dishes} alt="dishes" />
                                     <span style={{transform: "translate(60px, 100px)"}}>
-                                      <h2>Everyday From 10AM To 7PM</h2>
+                                      <h2>Everyday From 10AM To 9PM</h2>
                                       <h4>step in and Feel at Home</h4>
                                     </span>
                                 </div>
@@ -139,6 +177,7 @@ const IntroNavbar = () => {
                     <section>
                         <div className="menu-cards">
                             <div className="card-item event">
+                                <h1>for our loyal customers</h1>
                                 <h2><span data-aos="fade-left" data-aos-duration="600">Also Special </span>
                                     <span data-aos="zoom-out" data-aos-duration="800" style={{color:"#d00",fontSize:"1.5em"}}>Events</span> 
                                     <span data-aos="fade-right" data-aos-duration="600"> every 3 Month included our :</span> 
@@ -146,10 +185,11 @@ const IntroNavbar = () => {
                                 <ul>
                                     <li data-aos="fade-right">upComing talented singers</li>
                                     <li data-aos="fade-right">leading chefs</li>
+                                    <li data-aos="fade-right">special Offering from "chef tables" to "private dinner"</li>
                                     <li data-aos="fade-right">and Magicians</li>
                                 </ul>
                             </div>
-                            <p className="above-line" data-aos="zoom-out-down" data-aos-duration="900">Appetizers</p>
+                            <p id="menu" className="above-line" data-aos="zoom-out-down" data-aos-duration="900">Appetizers</p>
                             <span className="line" data-aos="slide-right" data-aos-duration="700"/>
                             <div className="card-item menu">
                                 <section>
@@ -398,18 +438,160 @@ const IntroNavbar = () => {
                     </section>
                 </div>
 
-                <div className="undefined-sections">
-                   <section data-aos="fade-up">
-                        <p>lorem Insum los immet sunm hcnjkd</p>
+                <div id="gallery" className="gallery-section">
+                    <section>
+                        <div className="gallery-textImages">
+                            <div className="image-one">
+                                <img src={beef} alt="beef" data-aos="fade-right" />
+                                <p data-aos="slide-up">
+                                The kinds of dishes to be produced obviously determine the jobs that need to be done. The menu is, 
+                                in fact, the basis for the entire operation.Because of its importance, we devote a whole chapter to
+                                a study of the menu to get the best flavor.
+                                </p>
+                            </div>
+                            <div id="burger" className="burger"> 
+                                <p data-aos="slide-up">
+                                Whether you’re in the mood for a <span>‘small’</span>  or a <span>‘big’</span> meal, 
+                                Zouj ou Haba is what you need to keep the day rolling,  or a double shot espresso, when it satisfies your birds, it’s all good to go.
+                                </p>
+                                <ul>
+                                    <li style={{transform:"translate(0, -3px)"}}><img src="http://vallierbistro.com/wp-content/themes/vallier/img/burger-1.png" alt="brg"/></li>
+                                    <li style={{transform:"translate(0, 7px)",opacity:".5"}}><img src="http://vallierbistro.com/wp-content/themes/vallier/img/burger-2.png" alt="brg"/></li>
+                                    <li style={{transform:"translate(0, 10px)"}}><img src="http://vallierbistro.com/wp-content/themes/vallier/img/burger-3.png" alt="brg"/></li>
+                                    <li style={{transform:"translate(0, 6px)",opacity:".7"}}><img src="http://vallierbistro.com/wp-content/themes/vallier/img/burger-4.png" alt="brg"/></li>
+                                    <li style={{transform:"translate(0, 1px)",opacity:".6"}}><img src="http://vallierbistro.com/wp-content/themes/vallier/img/burger-5.png" alt="brg"/></li>
+                                    <li><img src="http://vallierbistro.com/wp-content/themes/vallier/img/burger-6.png" alt="brg"/></li>
+                                </ul>
+                                <img src={zouj} alt="zouj"/>
+                            </div>
+                            <div className="image-two">
+                                <p data-aos="fade-left">
+                                The value of history is that it helps us understand the present and the future. In food service,
+                                knowledge of our professional heritage helps us see why we do things as we do, how our cooking techniques
+                                have been developed and refined,and how we can continue to develop and innovate in the years ahead.
+                                </p>
+                                <img data-aos="fade-right" src={handHold} alt="handhold" />
+                            </div>
+                        </div>
                     </section>
-                    <section data-aos="flip-left">
-                        <p>lorem Insum los immet sunm hcnjkd</p>
+                </div>
+
+                <div id="sercices" className="services-section">
+                    <section>
+                        <div className="service-textImages">
+                            <div className="image-one">
+                                <p data-aos="fade-up">zouj ou haba <span style={{color:"#d34",fontSize:".6em"}}>the way to go</span></p>
+                                <img data-aos="zoom-in" src={gathering} alt="gathering" />
+                                <p style={{fontSize:".5em"}}>Service with a Smile, Being Available, Being Exceptional</p>
+                            </div>
+                            <div className="image-two">
+                                <img data-aos="fade-up" data-aos-duration="1200" src={chairs} alt="chair" data-aos-delay="600"/>
+                                <p data-aos="fade-up" data-aos-duration="1200" data-aos-delay="600">
+                                    We work hard not only to give you the best flavor experience but also to let feel 
+                                    comfortable as if you are at home and beyond.
+                                </p>
+                                <ul data-aos="fade-down" data-aos-duration="2000" data-aos-delay="600">
+                                    Our Restaurant have Thousands of small items to let feel Home and beyon.
+                                    <li>cool location</li>
+                                    <li>amazing staff that are passionate of what we produce</li>
+                                    <li>Child friendly with larger table and high chairs and booster seats, coloring materials and toys</li>
+                                    <li>updated menu every 6 Month that keeps the kitchen team excited and creative</li>
+                                    <li>and our excellent platform to let you know About special events or extra tables and so on</li>
+                                </ul>
+                            </div>
+                            <div className="image-three">
+                                <div data-aos="fade-left" data-aos-duration="1200" data-aos-delay="600" className="romantic-text">
+                                <h2>and finlly for those of you who are confused of how to impress their wives or fiance</h2>
+                                <p>
+                                There's something special about getting engaged over a romantic dinner. But if you're wondering about 
+                                how to propose in a restaurant "Call and say, 'I'm considering getting engaged at your restaurant.
+                                and The staff will want to talk about what you have in mind, whether you've dined there before, 
+                                and what kind of vibe you'd like the engagement to have (fun and playful or romantic and meaningful?).
+                                </p>
+                                <h3>but if you have no idea in mind it's Alright</h3>
+                                <p>the staff will probably have some good ideas for proposing as well as some helpful tips</p>
+                                </div>
+                                <img data-aos="fade-right" data-aos-duration="1200" data-aos-delay="600" src={couple} alt="couple" />
+                            </div>
+                        </div>
                     </section>
-                    <section data-aos="fade-left">
-                        <p>lorem Insum los immet sunm hcnjkd</p>
+                </div>
+
+                <div id="about" className="about-section">
+                    <section>
+                        <div className="about-us">
+                            <div className="about-team">
+                                
+                            </div>
+                            <div className="about-text">
+                                <h1>FIND US</h1>
+                                <p>Ottawa Gloucester St</p>
+                                <p>Rue Laurier Ave W 48</p>
+                                <h3>Opening Hours</h3>
+                                <p> Mon-Tue-Wed 10:00AM - 10PM</p>
+                                <p>Thu-Fri-Sat 10:30 am-11 pm</p>
+                                <p>Sunday - closed (Jan to Mar)</p> 
+                                <p>11:30 am-10:00 (Apr to Dec)</p>
+                                <h3>RESERVATIONS</h3>
+                                <p><span style={{color:"rgb(0, 168, 0)",textDecoration:"underline"}}>514.842.2905</span> or <a href="http://zoujouhaba/book.com" target="blank">Book a table</a></p>
+                                <p style={{fontSize:".9em"}}>For more information</p> 
+                                <p><a href="http://info.com" target="blank">info@zoujouhab.com</a></p>
+                            </div>
+                            <div className="about-map">
+                                <ReactMapGl mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+                                    {...viewPort} onViewportChange={viewPort => { setViewPort(viewPort)}}
+                                    mapStyle="mapbox://styles/sidahmedzoldik/ck5mq44f32cfc1ipo6fvy86ct"
+                                    width="600px" height="450px" 
+                                    >
+                                    <div style={{position: 'absolute', right: 0}}>
+                                    <NavigationControl />
+                                    </div>
+
+                                    <Marker
+                                        key={"876785"}
+                                        latitude={45.4211}
+                                        longitude={-75.6945}
+                                        >
+                                        <button className="map-mark" onClick={e => {
+                                            e.preventDefault();
+                                            setSelectedHome("zouj ou haba wellcome you to be our next guest")
+                                        }}>
+                                            <img src ={zoujAdr} alt="address" width="88px"/>
+                                        </button>
+                                    </Marker>
+
+                                    {selecedHome ? (
+                                    <Popup      
+                                    latitude={45.4211}
+                                    longitude={-75.6945}
+                                    onClose={() => {
+                                        setSelectedHome(null)
+                                    }}
+                                    >
+                                        <p>{selecedHome}</p>
+                                    </Popup>
+                                    ) : null}
+                                </ReactMapGl>
+                            </div>
+                        </div>
                     </section>
-                    <section data-aos="zoom-out-down">
-                        <p>lorem Insum los immet sunm hcnjkd</p>
+                </div>
+
+                <div id="booking" className="booking-section">
+                    <section>
+                        <div className="find-us">
+                            <div className="item">
+                                <p>Website by boutaraa sidahmed freelancer and software_engineer.</p>
+                            </div>
+                            <div className="item">
+                                <p>Follow us on:</p>
+                                <a href="http://facebook.com" target="blank"><i className="fab fa-facebook"></i>facebook</a>
+                                <a href="http://instagram.com" target="blank"><i className="fab fa-instagram"></i>instagram</a>
+                            </div>
+                            <div className="item">
+                                <p>Copyright © 2020 Zouj_ou_Haba - All rights reserved</p>
+                            </div>
+                        </div>
                     </section>
                 </div>
 
